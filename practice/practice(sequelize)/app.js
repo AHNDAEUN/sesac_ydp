e485aa6d2693800fcf9b6e7b29f732080909cd5b
AHNDAEUN <dean034@naver.com>
2024-08-02 23:59:28 +0900

    진입점 파일 설정

--- practice/practice(sequelize)/app.js
+++ practice/practice(sequelize)/app.js
@@ -1,8 +1,36 @@
 const express= require('express');
 const { sequelize } = require('../../sequelize/models');
-const app= express();
-const port = 8888;
-
-// routes 등록
 
 const router = require('./routes')
+
+const express = require ('express') 
+const app = express();
+const router = require('./routes')
+
+const PORT = 8080;
+
+
+const {sequelize}= require('./models')
+
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+ app.use('/', router)
+
+
+
+ sequelize
+.sync({force: false})
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log('Database connection succeeded');
+        console.log(`Server is runnint! prot number is ${PORT}`);
+    });
+
+})
+
+.catch((err) => {
+    console.error(err);
+});
